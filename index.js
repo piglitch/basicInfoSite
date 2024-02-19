e90004a649475d6957eb31e57b785f1b02e73a05
Avi <avibanerjee2000@gmail.com>
2024-02-19 11:51:29 +0530

    implemented express

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const http = require('http');
-const fs = require('fs');
-//const path = require('path');
+const express = require("express");
+const app = express();
+const port = 4500;
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-   const filePath = req.url === '/' ? 'index.html' : req.url + '.html';
-   fs.readFile(filePath, (err, content) => {
-    if(err){
-        console.log('filepath: ', filePath, `req: ${req.url}`)
-        if (err.code == 'ENOENT') {
-            // Page not found
-            fs.readFile('404.html', (err, cont) =>{
-                res.writeHead(404, { 'content-Type': 'text/html' });
-                res.end(cont, 'utf8');
-            })
-        }        
-    } else{
-        console.log('filepath: ', filePath, `req: ${req.url}`)
-        res.writeHead(200, { 'content-Type': 'text/html' });
-        res.end(content);
-    }
-   })
+app.get("/", (req, res) => {
+    res.send("This is home");
 });
 
-const PORT = 5000;
+app.get("/about", (req, res) => {
+    const filePath = path.join(__dirname, "about.html");
+    res.sendFile(filePath);
+});
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/contact", (req, res) => {
+    const filePath = path.join(__dirname, "contact.html");
+    res.sendFile(filePath);
+});
+
+app.get("*", (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.listen(port, () => {
+    console.log('App is up at: ', 'http://localhost:4500/');
+});
